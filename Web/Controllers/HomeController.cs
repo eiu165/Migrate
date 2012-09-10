ee947e9f7ed4109275d5dc61452e811dddcbeb8e
nick <eiu165@gmail.com>
2012-09-09 20:49:54 -0500

    added some views

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Web.Models;
+
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+        private readonly AppDbContext _context = new AppDbContext();
+
+        public ActionResult Index()
+        {
+            return View(_context.Users);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+		public ActionResult Create(User user)
+		{
+			_context.Users.Add(user);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+	}
+}
